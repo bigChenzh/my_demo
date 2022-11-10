8c01be498c7b65d11297e44c9f54934ea65a9dcb
陈志浩 <chenzh@rabbitoise.com>
2022-11-10 22:48:31 +0800

    feat: day03 逻辑梳理并理解

--- mybatis_demo/src/test/java/cn/chen/day03/TestBean.java
+++ mybatis_demo/src/test/java/cn/chen/day03/TestBean.java
@@ -18,11 +18,17 @@ public class TestBean {
     @Test
     public void test_SqlSessionFactory() throws IOException {
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        //创建SqlSession工厂
+        //创建SqlSession工厂 获取一个配置xml
+        //将xml解析成一个configuration对象
+        //把这个configuration对象传递个SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(ResourceUtil.getReader("day03/IUserDao.xml", Charset.defaultCharset()));
+
         //通过工厂获取sqlSession对象
+        //SqlSessionFactory将configuration传递给SqlSession
         SqlSession sqlSession = sqlSessionFactory.openSession();
+
         //通过sqlSession获取集体实现代理类
+        //sqlSession通过配置类就可以产生代理对象了
         IUserDao userDao = sqlSession.getMapper(IUserDao.class);
 
         String s = userDao.queryUserInfoById("123");
